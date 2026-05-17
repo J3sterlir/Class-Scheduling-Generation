9ce4380c6fdc5140adcb7989bcef9730944da200
diVelarde <divelarde@gbox.adnu.edu.ph>
2026-05-18 04:53:06 +0800

    fix:bug error in cors

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(cors({
 }));
 
 // Handle preflight OPTIONS requests for all routes
-app.options("*", cors());
+app.options("/{*path}", cors());
 
 app.use(express.json());
 app.use(passport.initialize());
